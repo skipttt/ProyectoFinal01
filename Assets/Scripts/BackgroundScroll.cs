6786d10293359d09e7b3a95239afe82dca5153b1
Gianlucca <aimacedom1@outlook.com>
2025-05-19 14:20:49 -0600

    Escenario
    Nuevos fondos para el juego, aparte de perfeccionar las animaciones

--- Assets/Scripts/BackgroundScroll.cs
+++ Assets/Scripts/BackgroundScroll.cs
@@ -1,18 +1,52 @@
 using UnityEngine;
 
-
 public class BackgroundScroll : MonoBehaviour
 {
     public float scrollSpeed = 2f;
-    private Vector3 startPos;
+    private Transform[] backgrounds;
+    private float backgroundWidth;
 
-    void Start() => startPos = transform.position;
+    void Start()
+    {
+        // Obtén todos los hijos (fondos) en un arreglo
+        int childCount = transform.childCount;
+        backgrounds = new Transform[childCount];
+
+        for (int i = 0; i < childCount; i++)
+        {
+            backgrounds[i] = transform.GetChild(i);
+        }
+
+        // Suponemos que todos los fondos tienen el mismo ancho
+        SpriteRenderer sr = backgrounds[0].GetComponent<SpriteRenderer>();
+        if (sr != null)
+            backgroundWidth = sr.bounds.size.x;
+    }
 
     void Update()
     {
-        transform.Translate(Vector3.left * scrollSpeed * Time.deltaTime);
+        // Mueve cada fondo
+        foreach (Transform bg in backgrounds)
+        {
+            bg.Translate(Vector3.left * scrollSpeed * Time.deltaTime);
 
-        if (transform.position.x < -20) // Ajusta según tu fondo
-            transform.position = startPos;
+            // Si ya salió completamente de la vista, lo mandamos al final
+            if (bg.position.x <= -backgroundWidth)
+            {
+                float rightMostX = GetRightMostX();
+                bg.position = new Vector3(rightMostX + backgroundWidth, bg.position.y, bg.position.z);
+            }
+        }
+    }
+
+    float GetRightMostX()
+    {
+        float maxX = backgrounds[0].position.x;
+        foreach (Transform bg in backgrounds)
+        {
+            if (bg.position.x > maxX)
+                maxX = bg.position.x;
+        }
+        return maxX;
     }
 }
